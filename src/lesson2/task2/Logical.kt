084f9893c9b2e23524a0ab89f9593b32789ed8ab
burevgen <burdygin.evgenii@gmail.com>
2022-11-09 20:55:41 +0300

    1-5

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -42,7 +42,7 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean =
  */
 fun daysInMonth(month: Int, year: Int): Int {
     var k = 0
-    k = if (((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))) {
+    k = if (((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) && month==2) {
         29
     } else {
         when (month) {
